a1ebb8be6d0d735de2f12e5287cd38d6a7e1ec8d
Zakaria El Omari <148917149+ZAKARIA-ELOMARI@users.noreply.github.com>
2025-08-03 22:43:46 +0100

    Delete aegis-chat-frontend directory

--- aegis-chat-frontend/electron/preload.ts
+++
@@ -1,8 +0,0 @@
-// In electron/preload.ts
-import { contextBridge } from 'electron';
-
-// Expose a secure API to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  // We will add functions here later for things like file system access
-  // or other OS-level interactions that the React app shouldn't do directly.
-});
